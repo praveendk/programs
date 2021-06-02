84b47a04931295d92bd44c6bd725cf1f0b41c6c8
unknown <vlnrdtuoh@gmail.com>
2021-06-02 18:36:15 +0530

    Missing Letters program added

---
+++ lists/Typing.py
@@ -0,0 +1,4 @@
+#using list in the program
+list = ["psychology", "philosophy", "biology", "social science", "science"]
+for i in list:
+	print(i)
---
+++ lists/missingLetters.py
@@ -0,0 +1,16 @@
+#program of missing letters
+#for loop and list
+print("Welcome to Missing letters program! \n")
+userName = input("Please enter your name: \n")
+print("hi " + userName + ", Welcome to the program!")
+emailAddress = input("Please enter your email address: \n")
+print("Thanks " + userName + " for providing your email address.")
+missingLetters = ["di-b-t-s", "-n-ul-n", "g--c-s-", "nu--i-i-n"]
+correctWords = ["diabetes", "insulin", "glucose", "nutrition"]
+for i in missingLetters:
+	print("Fill the blanks with appropriate letters to make meaningful word \n" + i)
+	userAnswer = input()
+	if (userAnswer == correctWords):
+		print("You got it right! ")
+	else:
+		print("Oh, wrong answer!")
